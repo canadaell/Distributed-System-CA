737c6f26a007537c5ab6a9b3b10ee34d2c44695b
Mingde <mingdezhou1@outlook.com>
2024-04-18 12:36:59 +0100

    add open csv, write sensor service

--- src/main/java/grpc/SensorService/SensorServiceImpl.java
+++ src/main/java/grpc/SensorService/SensorServiceImpl.java
@@ -1,5 +1,32 @@
 package grpc.SensorService;
 
+import io.grpc.stub.StreamObserver;
+import grpc.SensorService.AddSensorResponse;
+
 public class SensorServiceImpl extends SensorServiceGrpc.SensorServiceImplBase {
+    @Override
+    public void addSensor(AddSensorRequest request, StreamObserver<AddSensorResponse> responseObserver) {
+        int sensorId = request.getSensorid();
+        String sensorType = request.getSensortype();
+        String location = request.getLocation();
+
+        boolean success = true;
+        String message = "Sensor added successfully, " + "sensor ID " + request.getSensorid() + ", sensor type: " + request.getSensortype() + ", location: " + request.getLocation();
+
+        AddSensorResponse response = AddSensorResponse.newBuilder()
+                .setSuccess(success)
+                .setMessage(message)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+
+    @Override
+    public void streamSensorData(SensorRequest request,
+                                 StreamObserver<SensorResponse> responseObserver) {
+
+    }
 
 }
