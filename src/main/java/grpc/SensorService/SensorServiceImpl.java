4a5b55e40179eb49c22e15f77e7a4c69c3dfe67e
Mingde <mingdezhou1@outlook.com>
2024-04-18 13:27:07 +0100

    write sensor service

--- src/main/java/grpc/SensorService/SensorServiceImpl.java
+++ src/main/java/grpc/SensorService/SensorServiceImpl.java
@@ -3,6 +3,8 @@ package grpc.SensorService;
 import io.grpc.stub.StreamObserver;
 import grpc.SensorService.AddSensorResponse;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class SensorServiceImpl extends SensorServiceGrpc.SensorServiceImplBase {
     @Override
     public void addSensor(AddSensorRequest request, StreamObserver<AddSensorResponse> responseObserver) {
@@ -24,9 +26,38 @@ public class SensorServiceImpl extends SensorServiceGrpc.SensorServiceImplBase {
 
 
     @Override
-    public void streamSensorData(SensorRequest request,
-                                 StreamObserver<SensorResponse> responseObserver) {
+    public void streamSensorData(SensorRequest request, StreamObserver<SensorResponse> responseObserver) {
+        int farmId = request.getFarmid();
+        int sensorId = request.getSensorid();
 
+        Runnable streamingTask = () -> {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+
+                    double temperature = ThreadLocalRandom.current().nextDouble(15.0, 35.0);
+                    double humidity = ThreadLocalRandom.current().nextDouble(40.0, 80.0);
+                    double illumination = ThreadLocalRandom.current().nextDouble(0.0, 1000.0);
+
+                    SensorResponse response = SensorResponse.newBuilder()
+                            .setTempreture(temperature)
+                            .setHumidity(humidity)
+                            .setDistrict("District " + farmId)
+                            .setIllumination(illumination)
+                            .build();
+
+                    responseObserver.onNext(response);
+                    Thread.sleep(5000); // Stream every 5 seconds
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } finally {
+                responseObserver.onCompleted();
+            }
+        };
+
+        new Thread(streamingTask).start();
     }
 
 }
+
+
